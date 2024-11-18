5f6d07c8eb17a09a4af7c0412bcabd27c77394a0
Felix Touchte Codjo <touchte@ifarm2401.jlab.org>
2024-11-18 06:28:29 -0500

    Reduce indentation

--- common-tools/clas-detector/src/main/java/org/jlab/detector/pulse/ModeAHDC.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/pulse/ModeAHDC.java
@@ -126,13 +126,13 @@ public class ModeAHDC extends HipoExtractor  {
 	 * @param samplingTime time between 2 ADC bins
 	 */
 	private void fitAverage(float samplingTime){
-			if ((binMax - 2 >= 0) && (binMax + 2 <= binNumber - 1)){
-					adcMax = 0;
-					for (int bin = binMax - 2; bin <= binMax + 2; bin++){
-							adcMax += samplesCorr[bin];
-					}
-					adcMax = adcMax/5;
+		if ((binMax - 2 >= 0) && (binMax + 2 <= binNumber - 1)){
+			adcMax = 0;
+			for (int bin = binMax - 2; bin <= binMax + 2; bin++){
+				adcMax += samplesCorr[bin];
 			}
+			adcMax = adcMax/5;
+		}
 	}
 
 	/**
@@ -150,13 +150,13 @@ public class ModeAHDC extends HipoExtractor  {
 	 * @param fineTimeStampResolution precision of dream clock (usually 8)
 	 */
 	private void fineTimeStampCorrection (long timeStamp, float fineTimeStampResolution) {
-			this.timestamp = timeStamp;
-			String binaryTimeStamp = Long.toBinaryString(timeStamp); //get 64 bit timestamp in binary format
-			if (binaryTimeStamp.length()>=3){
-					byte fineTimeStamp = Byte.parseByte(binaryTimeStamp.substring(binaryTimeStamp.length()-3,binaryTimeStamp.length()),2); //fineTimeStamp : keep and convert last 3 bits of binary timestamp
-					timeMax += (float) ((fineTimeStamp+0.5) * fineTimeStampResolution); //fineTimeStampCorrection
-					// Question : I wonder if I have to do the same thing of all time quantities that the extract() methods compute. 
-			}
+		this.timestamp = timeStamp;
+		String binaryTimeStamp = Long.toBinaryString(timeStamp); //get 64 bit timestamp in binary format
+		if (binaryTimeStamp.length()>=3){
+			byte fineTimeStamp = Byte.parseByte(binaryTimeStamp.substring(binaryTimeStamp.length()-3,binaryTimeStamp.length()),2); //fineTimeStamp : keep and convert last 3 bits of binary timestamp
+			timeMax += (float) ((fineTimeStamp+0.5) * fineTimeStampResolution); //fineTimeStampCorrection
+			// Question : I wonder if I have to do the same thing of all time quantities that the extract() methods compute. 
+		}
 	}
 
 	/**
@@ -166,38 +166,38 @@ public class ModeAHDC extends HipoExtractor  {
 	 * @param amplitudeFraction amplitude fraction between 0 and 1
 	 */
 	private void computeTimeAtConstantFractionAmplitude(float samplingTime, float amplitudeFractionCFA){
-			float threshold = amplitudeFractionCFA*adcMax;
-			// timeRiseCFA
-			int binRise = 0;
-			for (int bin = 0; bin < binMax; bin++){
-					if (samplesCorr[bin] < threshold)
-							binRise = bin;  // last pass below threshold and before adcMax
-			} // at this stage : binRise < timeRiseCFA/samplingTime <= binRise + 1 // timeRiseCFA is determined by assuming a linear fit between binRise and binRise + 1
-			float slopeRise = 0;
-			if (binRise + 1 <= binNumber-1)
-					slopeRise = samplesCorr[binRise+1] - samplesCorr[binRise];
-			float fittedBinRise = (slopeRise == 0) ? binRise : binRise + (threshold - samplesCorr[binRise])/slopeRise;
-			timeRiseCFA = (fittedBinRise + binOffset)*samplingTime; // binOffset is determined in wavefromCorrection() // must be the same for all time ? // or must be defined using fittedBinRise*sparseSample
+		float threshold = amplitudeFractionCFA*adcMax;
+		// timeRiseCFA
+		int binRise = 0;
+		for (int bin = 0; bin < binMax; bin++){
+			if (samplesCorr[bin] < threshold)
+					binRise = bin;  // last pass below threshold and before adcMax
+		} // at this stage : binRise < timeRiseCFA/samplingTime <= binRise + 1 // timeRiseCFA is determined by assuming a linear fit between binRise and binRise + 1
+		float slopeRise = 0;
+		if (binRise + 1 <= binNumber-1)
+			slopeRise = samplesCorr[binRise+1] - samplesCorr[binRise];
+		float fittedBinRise = (slopeRise == 0) ? binRise : binRise + (threshold - samplesCorr[binRise])/slopeRise;
+		timeRiseCFA = (fittedBinRise + binOffset)*samplingTime; // binOffset is determined in wavefromCorrection() // must be the same for all time ? // or must be defined using fittedBinRise*sparseSample
 
-			// timeFallCFA
-			int binFall = binMax;
-			for (int bin = binMax; bin < binNumber; bin++){
-					if (samplesCorr[bin] > threshold){
-							binFall = bin;
-					}
-					else {
-							binFall = bin;
-							break; // first pass below the threshold
-					}
-			} // at this stage : binFall - 1 <= timeRiseCFA/samplingTime < binFall // timeFallCFA is determined by assuming a linear fit between binFall - 1 and binFall
-			float slopeFall = 0;
-			if (binFall - 1 >= 0)
-					slopeFall = samplesCorr[binFall] - samplesCorr[binFall-1];
-			float fittedBinFall = (slopeFall == 0) ? binFall : binFall-1 + (threshold - samplesCorr[binFall-1])/slopeFall;
-			timeFallCFA = (fittedBinFall + binOffset)*samplingTime;
+		// timeFallCFA
+		int binFall = binMax;
+		for (int bin = binMax; bin < binNumber; bin++){
+			if (samplesCorr[bin] > threshold){
+				binFall = bin;
+			}
+			else {
+				binFall = bin;
+				break; // first pass below the threshold
+			}
+		} // at this stage : binFall - 1 <= timeRiseCFA/samplingTime < binFall // timeFallCFA is determined by assuming a linear fit between binFall - 1 and binFall
+		float slopeFall = 0;
+		if (binFall - 1 >= 0)
+			slopeFall = samplesCorr[binFall] - samplesCorr[binFall-1];
+		float fittedBinFall = (slopeFall == 0) ? binFall : binFall-1 + (threshold - samplesCorr[binFall-1])/slopeFall;
+		timeFallCFA = (fittedBinFall + binOffset)*samplingTime;
 
-			// timeOverThreshold
-			timeOverThresholdCFA = timeFallCFA - timeRiseCFA;
+		// timeOverThreshold
+		timeOverThresholdCFA = timeFallCFA - timeRiseCFA;
 	}
 
 	/**
@@ -208,35 +208,35 @@ public class ModeAHDC extends HipoExtractor  {
 	 * @param binDelayCFD CFD delay parameter
 	 */
 	private void computeTimeUsingConstantFractionDiscriminator(float samplingTime, float fractionCFD, int binDelayCFD){
-			float[] signal = new float[binNumber];
-			// signal generation
-			for (int bin = 0; bin < binNumber; bin++){
-					signal[bin] = (1 - fractionCFD)*samplesCorr[bin]; // we fill it with a fraction of the original signal
-					if (bin < binNumber - binDelayCFD)
-							signal[bin] += -1*fractionCFD*samplesCorr[bin + binDelayCFD]; // we advance and invert a complementary fraction of the original signal and superimpose it to the previous signal
-			}
-			// determine the two humps
-			int binHumpSup = 0;
-			int binHumpInf = 0;
-			for (int bin = 0; bin < binNumber; bin++){
-					if (signal[bin] > signal[binHumpSup])
-							binHumpSup = bin;
-			}
-			for (int bin = 0; bin < binHumpSup; bin++){ // this loop has been added to be sure : binHumpInf < binHumpSup
-					if (signal[bin] < signal[binHumpInf])
-							binHumpInf = bin;
-			}
-			// research for zero
-			int binZero = 0;
-			for (int bin = binHumpInf; bin <= binHumpSup; bin++){
-					if (signal[bin] < 0)
-							binZero = bin; // last pass below zero
-			} // at this stage : binZero < timeCFD/samplingTime <= binZero + 1 // timeCFD is determined by assuming a linear fit between binZero and binZero + 1
-			float slopeCFD = 0;
-			if (binZero + 1 <= binNumber)
-					slopeCFD = signal[binZero+1] - signal[binZero];
-			float fittedBinZero = (slopeCFD == 0) ? binZero : binZero + (0 - signal[binZero])/slopeCFD;
-			timeCFD = (fittedBinZero + binOffset)*samplingTime;
+		float[] signal = new float[binNumber];
+		// signal generation
+		for (int bin = 0; bin < binNumber; bin++){
+			signal[bin] = (1 - fractionCFD)*samplesCorr[bin]; // we fill it with a fraction of the original signal
+			if (bin < binNumber - binDelayCFD)
+				signal[bin] += -1*fractionCFD*samplesCorr[bin + binDelayCFD]; // we advance and invert a complementary fraction of the original signal and superimpose it to the previous signal
+		}
+		// determine the two humps
+		int binHumpSup = 0;
+		int binHumpInf = 0;
+		for (int bin = 0; bin < binNumber; bin++){
+			if (signal[bin] > signal[binHumpSup])
+				binHumpSup = bin;
+		}
+		for (int bin = 0; bin < binHumpSup; bin++){ // this loop has been added to be sure : binHumpInf < binHumpSup
+			if (signal[bin] < signal[binHumpInf])
+				binHumpInf = bin;
+		}
+		// research for zero
+		int binZero = 0;
+		for (int bin = binHumpInf; bin <= binHumpSup; bin++){
+			if (signal[bin] < 0)
+						binZero = bin; // last pass below zero
+		} // at this stage : binZero < timeCFD/samplingTime <= binZero + 1 // timeCFD is determined by assuming a linear fit between binZero and binZero + 1
+		float slopeCFD = 0;
+		if (binZero + 1 <= binNumber)
+			slopeCFD = signal[binZero+1] - signal[binZero];
+		float fittedBinZero = (slopeCFD == 0) ? binZero : binZero + (0 - signal[binZero])/slopeCFD;
+		timeCFD = (fittedBinZero + binOffset)*samplingTime;
 
 	}
 
