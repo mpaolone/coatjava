bde82462ca3a9fc2ab7483f014d8e8867be6040f
Churamani Paudel <churaman@ifarm2401.jlab.org>
2025-01-04 22:05:18 -0500

    update on plot packages

--- reconstruction/alert/src/main/java/org/jlab/rec/atof/ATOF_RECON/src/main/java/org/example/ZAndPhiForBarsZPositionAndTimePlotter.java
+++ reconstruction/alert/src/main/java/org/jlab/rec/atof/ATOF_RECON/src/main/java/org/example/ZAndPhiForBarsZPositionAndTimePlotter.java
@@ -3,6 +3,7 @@ package org.jlab.rec.atof.ATOF_RECON;
 import org.jlab.jnp.hipo4.data.Bank;
 import org.jlab.jnp.hipo4.data.Event;
 import org.jlab.jnp.hipo4.io.HipoReader;
+
 /*
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -11,6 +12,15 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 */
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.data.statistics.HistogramDataset;
+
 
 import javax.swing.*;
 import java.util.ArrayList;
